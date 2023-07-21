daf63babc3c1035479e2da0fb097da48077d92f5
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-07-22 01:22:28 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -10,21 +10,26 @@ function NavBar() {
 
 function Home() {
   return (
-    <div id="home">
-      <h1>Home</h1>
+    <div id="about">
+      <h2>Home</h2>
     </div>
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="home">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
